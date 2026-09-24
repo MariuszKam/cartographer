984a81bbecbd1e71e9d68c41e2c0e3ea37bed5ca
Mariusz <87607813+MariuszKam@users.noreply.github.com>
2026-09-24 21:07:24 +0200

    fix: restore Objects import after builder cleanup

--- src/main/java/cartographer/resource/SurfaceObjectCandidateCatalogBuilder.java
+++ src/main/java/cartographer/resource/SurfaceObjectCandidateCatalogBuilder.java
@@ -6,6 +6,7 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
