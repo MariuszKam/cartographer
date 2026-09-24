61769ff506580147f155276f04e0d8ce5c6f2ae1
Mariusz <87607813+MariuszKam@users.noreply.github.com>
2026-09-24 20:10:08 +0200

    cleanup: remove disconnected performance subsystems

--- src/main/java/cartographer/perf/instrumentation/PerformanceStageScope.java
+++
@@ -1,8 +0,0 @@
-package cartographer.perf.instrumentation;
-
-/** Coarse stage timing scope; close it with try-with-resources. */
-@FunctionalInterface
-public interface PerformanceStageScope extends AutoCloseable {
-    @Override
-    void close();
-}
